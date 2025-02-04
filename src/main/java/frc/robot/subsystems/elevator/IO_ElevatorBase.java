554722b203708583110cf76c60fb846e3628b494
Emre <kozanfurkanemre@gmail.com>
2025-02-04 19:43:27 +0300

    bazı açıklamalar

--- src/main/java/frc/robot/subsystems/elevator/IO_ElevatorBase.java
+++ src/main/java/frc/robot/subsystems/elevator/IO_ElevatorBase.java
@@ -4,24 +4,54 @@ import org.littletonrobotics.junction.AutoLog;
 
 public interface IO_ElevatorBase {
 
-  @AutoLog
-  public static class ElevatorInputs {
-    public double[] elevatorAppliedVolts = new double[] {};
-    public double[] elevatorCurrentAmps = new double[] {};
-    public double[] elevatorTempCelcius = new double[] {};
-    public double elevatorPositionRad = 0.0;
-    public double elevatorVelocityRadPerSec = 0.0;
-  }
+    @AutoLog
+    public static class ElevatorInputs {
+        public double[] elevatorAppliedVolts = new double[] {};
+        public double[] elevatorCurrentAmps = new double[] {};
+        public double[] elevatorTempCelcius = new double[] {};
+        public double elevatorPositionRad = 0.0;
+        public double elevatorVelocityRadPerSec = 0.0;
+    }
 
-  void updateInputs(ElevatorInputs inputs);
+    void updateInputs(ElevatorInputs inputs);
 
-  public void setElevatorVoltage(double volts);
+    /**
+     * Asansör motorlarının voltajını ayarlar
+     * 
+     * @param volts Çalıştırmak istediğiniz volt değeri.
+     */
+    public void setElevatorVoltage(double volts);
 
-  public void setSlot0(double kG, double kS, double kV, double kA, double kP, double kI, double kD);
+    /**
+     * PID kontrolünde kullanılacak sabitler
+     * 
+     * @param kG
+     * @param kS
+     * @param kV
+     * @param kA
+     * @param kP
+     * @param kI
+     * @param kD
+     */
+    public void setSlot0(double kG, double kS, double kV, double kA, double kP, double kI,
+        double kD);
 
-  public void stopMotor();
+    /**
+     * Asansör motorlarını durdurur.
+     */
+    public void stopMotor();
 
-  public void runPosition(double positionRad);
+    /**
+     * Asansörü belli bir pozisyona götürür.
+     * 
+     * @param positionRad başlangıç konumundan motorun radyan cinsinden sabitlenmek istendiği konum.
+     */
+    public void runPosition(double positionRad);
 
-  public boolean isPositionWithinLimits(double positionRad);
+    /**
+     * Asansöre yazılımsal limit ekler.
+     * 
+     * @param positionRad Asansörün gitmek istediği konum
+     */
+    public boolean isPositionWithinLimits(double positionRad);
 }
--- src/main/java/frc/robot/subsystems/elevator/SUB_Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/SUB_Elevator.java
@@ -11,74 +11,95 @@ import org.littletonrobotics.junction.Logger;
 
 public class SUB_Elevator extends SubsystemBase {
 
-  private static final LoggedTunableNumber kG = new LoggedTunableNumber("Elevator/kG", 0);
-  private static final LoggedTunableNumber kS = new LoggedTunableNumber("Elevator/kS", 0);
-  private static final LoggedTunableNumber kV = new LoggedTunableNumber("Elevator/kV", 0);
-  private static final LoggedTunableNumber kA = new LoggedTunableNumber("Elevator/kA", 0);
-  private static final LoggedTunableNumber kP = new LoggedTunableNumber("Elevator/kP", 0);
-  private static final LoggedTunableNumber kI = new LoggedTunableNumber("Elevator/kI", 0);
-  private static final LoggedTunableNumber kD = new LoggedTunableNumber("Elevator/kD", 0);
+    private static final LoggedTunableNumber kG = new LoggedTunableNumber("Elevator/kG", 0);
+    private static final LoggedTunableNumber kS = new LoggedTunableNumber("Elevator/kS", 0);
+    private static final LoggedTunableNumber kV = new LoggedTunableNumber("Elevator/kV", 0);
+    private static final LoggedTunableNumber kA = new LoggedTunableNumber("Elevator/kA", 0);
+    private static final LoggedTunableNumber kP = new LoggedTunableNumber("Elevator/kP", 0);
+    private static final LoggedTunableNumber kI = new LoggedTunableNumber("Elevator/kI", 0);
+    private static final LoggedTunableNumber kD = new LoggedTunableNumber("Elevator/kD", 0);
 
-  private static SysIdRoutine sysIdRoutine;
+    private static SysIdRoutine sysIdRoutine;
 
-  private final IO_ElevatorBase io;
-  public final ElevatorInputsAutoLogged inputs = new ElevatorInputsAutoLogged();
+    private final IO_ElevatorBase io;
+    public final ElevatorInputsAutoLogged inputs = new ElevatorInputsAutoLogged();
 
-  public SUB_Elevator(IO_ElevatorBase io) {
-    this.io = io;
+    public SUB_Elevator(IO_ElevatorBase io)
+    {
+        this.io = io;
 
-    sysIdRoutine =
-        new SysIdRoutine(
-            new SysIdRoutine.Config(
-                null, // Use default ramp rate (1 V/s)
-                Volts.of(4), // Reduce dynamic step voltage to 4 to prevent brownout
-                null, // Use default timeout (10 s)
-                // Log state with Phoenix SignalLogger class
-                (state) -> SignalLogger.writeString("state", state.toString())),
-            new SysIdRoutine.Mechanism(
-                (volts) -> io.setElevatorVoltage(volts.in(Volts)), null, this));
-  }
-
-  @Override
-  public void periodic() {
-    io.updateInputs(inputs);
-    Logger.processInputs("Elevator", inputs);
-    /*
-     * if(!io.isPositionWithinLimits(inputs.elevatorPositionRad)) { io.stopMotor(); }
-     */
-
-    // Update tunable numbers
-    if (kG.hasChanged(hashCode())
-        || kS.hasChanged(hashCode())
-        || kV.hasChanged(hashCode())
-        || kA.hasChanged(hashCode())
-        || kP.hasChanged(hashCode())
-        || kI.hasChanged(hashCode())
-        || kD.hasChanged(hashCode())) {
-      io.setSlot0(kG.get(), kS.get(), kV.get(), kA.get(), kP.get(), kI.get(), kD.get());
+        sysIdRoutine =
+            new SysIdRoutine(
+                new SysIdRoutine.Config(
+                    null, // Use default ramp rate (1 V/s)
+                    Volts.of(4), // Reduce dynamic step voltage to 4 to prevent brownout
+                    null, // Use default timeout (10 s)
+                    // Log state with Phoenix SignalLogger class
+                    (state) -> SignalLogger.writeString("state", state.toString())),
+                new SysIdRoutine.Mechanism(
+                    (volts) -> io.setElevatorVoltage(volts.in(Volts)), null, this));
     }
-  }
 
-  @Override
-  public void simulationPeriodic() {}
+    @Override
+    public void periodic()
+    {
+        io.updateInputs(inputs);
+        Logger.processInputs("Elevator", inputs);
+        /*
+         * if(!io.isPositionWithinLimits(inputs.elevatorPositionRad)) { io.stopMotor(); }
+         */
 
-  public void setElevatorVoltage(double volts) {
-    io.setElevatorVoltage(volts);
-  }
+        // Update tunable numbers
+        if (kG.hasChanged(hashCode())
+            || kS.hasChanged(hashCode())
+            || kV.hasChanged(hashCode())
+            || kA.hasChanged(hashCode())
+            || kP.hasChanged(hashCode())
+            || kI.hasChanged(hashCode())
+            || kD.hasChanged(hashCode())) {
+            io.setSlot0(kG.get(), kS.get(), kV.get(), kA.get(), kP.get(), kI.get(), kD.get());
+        }
+    }
 
-  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
-    return sysIdRoutine.quasistatic(direction);
-  }
+    @Override
+    public void simulationPeriodic()
+    {}
 
-  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
-    return sysIdRoutine.dynamic(direction);
-  }
+    /**
+     * Asansör motorlarının voltajını ayarlar
+     * 
+     * @param volts Çalıştırmak istediğiniz volt değeri.
+     */
+    public void setElevatorVoltage(double volts)
+    {
+        io.setElevatorVoltage(volts);
+    }
 
-  public void runPosition(double positionRad) {
-    io.runPosition(positionRad);
-  }
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction)
+    {
+        return sysIdRoutine.quasistatic(direction);
+    }
 
-  public void stopElevator() {
-    io.stopMotor();
-  }
+    public Command sysIdDynamic(SysIdRoutine.Direction direction)
+    {
+        return sysIdRoutine.dynamic(direction);
+    }
+
+    /**
+     * Asansörü belli bir pozisyona götürür.
+     * 
+     * @param positionRad başlangıç konumundan motorun radyan cinsinden sabitlenmek istendiği konum.
+     */
+    public void runPosition(double positionRad)
+    {
+        io.runPosition(positionRad);
+    }
+
+    /**
+     * Asansör motorlarını durdurur.
+     */
+    public void stopElevator()
+    {
+        io.stopMotor();
+    }
 }
